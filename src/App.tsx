e5202933c378258b4bdfe9cefe01c8a4e7870fbc
gianniresende <gianniresende@gmail.com>
2020-06-08 07:37:09 -0300

    feat: add form create_point

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes'
-import CreatePoint from './pages/create_point'
 function App() {
   return (
     < Routes />
   );
 }
-
 export default App;
